8203c7f058b2426d8445bc2cc569893ebd274843
Cintia Solange Godoy Szkafarowski <CintiaSolange.GodoySzkafarowski@Alumnos.uai.edu.ar>
2021-09-22 17:00:33 -0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,349 @@
+﻿
+namespace Tp_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.CategoriesTab = new System.Windows.Forms.TabPage();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ShippersTab = new System.Windows.Forms.TabPage();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.CategoriesTab.SuspendLayout();
+            this.ShippersTab.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dataGridView1.Location = new System.Drawing.Point(15, 163);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(706, 223);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.DoubleClick += new System.EventHandler(this.dataGridView1_DoubleClick);
+            // 
+            // Column1
+            // 
+            this.Column1.Frozen = true;
+            this.Column1.HeaderText = "Category Name";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 125;
+            // 
+            // Column2
+            // 
+            this.Column2.Frozen = true;
+            this.Column2.HeaderText = "Description";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 125;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.CategoriesTab);
+            this.tabControl1.Controls.Add(this.ShippersTab);
+            this.tabControl1.Location = new System.Drawing.Point(3, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(796, 450);
+            this.tabControl1.TabIndex = 1;
+            this.tabControl1.Click += new System.EventHandler(this.tabControl1_Click);
+            // 
+            // CategoriesTab
+            // 
+            this.CategoriesTab.Controls.Add(this.button3);
+            this.CategoriesTab.Controls.Add(this.button2);
+            this.CategoriesTab.Controls.Add(this.button1);
+            this.CategoriesTab.Controls.Add(this.textBox2);
+            this.CategoriesTab.Controls.Add(this.label2);
+            this.CategoriesTab.Controls.Add(this.textBox1);
+            this.CategoriesTab.Controls.Add(this.label1);
+            this.CategoriesTab.Controls.Add(this.dataGridView1);
+            this.CategoriesTab.Location = new System.Drawing.Point(4, 25);
+            this.CategoriesTab.Name = "CategoriesTab";
+            this.CategoriesTab.Padding = new System.Windows.Forms.Padding(3);
+            this.CategoriesTab.Size = new System.Drawing.Size(788, 421);
+            this.CategoriesTab.TabIndex = 0;
+            this.CategoriesTab.Text = "Categories";
+            this.CategoriesTab.UseVisualStyleBackColor = true;
+            this.CategoriesTab.Click += new System.EventHandler(this.CategoriesTab_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(280, 99);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Update";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(146, 99);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Delate";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 99);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Insert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(146, 50);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Description";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(146, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Category Name";
+            // 
+            // ShippersTab
+            // 
+            this.ShippersTab.Controls.Add(this.button4);
+            this.ShippersTab.Controls.Add(this.button5);
+            this.ShippersTab.Controls.Add(this.button6);
+            this.ShippersTab.Controls.Add(this.textBox3);
+            this.ShippersTab.Controls.Add(this.label3);
+            this.ShippersTab.Controls.Add(this.textBox4);
+            this.ShippersTab.Controls.Add(this.label4);
+            this.ShippersTab.Controls.Add(this.dataGridView2);
+            this.ShippersTab.Location = new System.Drawing.Point(4, 25);
+            this.ShippersTab.Name = "ShippersTab";
+            this.ShippersTab.Padding = new System.Windows.Forms.Padding(3);
+            this.ShippersTab.Size = new System.Drawing.Size(788, 421);
+            this.ShippersTab.TabIndex = 1;
+            this.ShippersTab.Text = "Shippers";
+            this.ShippersTab.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(306, 110);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "Update";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(172, 110);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 16;
+            this.button5.Text = "Delate";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(51, 110);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 15;
+            this.button6.Text = "Insert";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(172, 61);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 17);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Phone";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(172, 23);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(48, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 17);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Company Name";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2});
+            this.dataGridView2.Location = new System.Drawing.Point(41, 174);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(706, 223);
+            this.dataGridView2.TabIndex = 10;
+            this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
+            this.dataGridView2.Click += new System.EventHandler(this.dataGridView2_Click);
+            this.dataGridView2.DoubleClick += new System.EventHandler(this.dataGridView2_DoubleClick);
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.Frozen = true;
+            this.dataGridViewTextBoxColumn1.HeaderText = "Company Name";
+            this.dataGridViewTextBoxColumn1.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
+            this.dataGridViewTextBoxColumn1.Width = 125;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.Frozen = true;
+            this.dataGridViewTextBoxColumn2.HeaderText = "Phone";
+            this.dataGridViewTextBoxColumn2.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.ReadOnly = true;
+            this.dataGridViewTextBoxColumn2.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Tp4";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.CategoriesTab.ResumeLayout(false);
+            this.CategoriesTab.PerformLayout();
+            this.ShippersTab.ResumeLayout(false);
+            this.ShippersTab.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage CategoriesTab;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage ShippersTab;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entities;
+using Logic;
+namespace Tp_4
+{
+    public partial class Form1 : Form
+    {
+        private ProductLogic BaseDatos;
+        List<Categories> Categorias;
+        List<Shippers> Cargadores;
+
+        public Form1()
+        {
+            InitializeComponent();
+            BaseDatos = new ProductLogic();
+            refreshCategories();
+            refreshShippers();
+            
+          
+        }
+        private void refreshCategories()
+        {
+            Categorias = BaseDatos.GetCategories();
+            textBox1.Text = "";
+            textBox2.Text = "";
+            dataGridView1.Rows.Clear();
+            foreach (Categories cat in Categorias)
+            {
+                dataGridView1.Rows.Add(new string[] { cat.CategoryName, cat.Description });
+            }
+        }
+        private void refreshShippers()
+        {
+            Cargadores = BaseDatos.GetShippers();
+            textBox3.Text = "";
+            textBox4.Text = "";
+            dataGridView2.Rows.Clear();
+            foreach (Shippers ship in Cargadores)
+            {
+                dataGridView2.Rows.Add(new string[] { ship.CompanyName, ship.Phone });
+            }
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CategoriesTab_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_DoubleClick(object sender, EventArgs e)
+        {
+            Categories Cat = Categorias[dataGridView1.CurrentCell.RowIndex];
+            textBox1.Text = Cat.CategoryName;
+            textBox2.Text = Cat.Description;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Categories Cat = new Categories();
+            Cat.CategoryName = textBox1.Text;
+            Cat.Description = textBox2.Text;
+            if (BaseDatos.AddCategory(Cat))
+                refreshCategories();
+            else
+                MessageBox.Show("No se pudo agregar el elemento");
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentCell.RowIndex > 0)
+            {
+                BaseDatos.DeleteCategory(Categorias[dataGridView1.CurrentCell.RowIndex]);
+                refreshCategories();
+            }
+            else
+                MessageBox.Show("Debe elegir un elemento haciendo doble clic en la tabla");
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Categories Cat = Categorias[dataGridView1.CurrentCell.RowIndex];
+            Cat.CategoryName = textBox1.Text;
+            Cat.Description = textBox2.Text;
+            BaseDatos.UpdateCategory(Cat);
+            refreshCategories();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Shippers Ship = new Shippers();
+            Ship.CompanyName = textBox4.Text;
+            Ship.Phone = textBox3.Text;
+            if (BaseDatos.AddShippers(Ship))
+                refreshShippers();
+            else
+                MessageBox.Show("No se pudo agregar el elemento");
+
+            
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (BaseDatos.DeleteShippers(Cargadores[dataGridView2.CurrentCell.RowIndex]))
+                refreshShippers();
+            else
+                MessageBox.Show("No se puede eliminar el elemento");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Shippers Ship = Cargadores[dataGridView2.CurrentCell.RowIndex];
+            Ship.CompanyName = textBox4.Text;
+           Ship.Phone = textBox3.Text;
+            if (BaseDatos.UpdateShippers(Ship))
+                refreshShippers();
+            else
+                MessageBox.Show("No se puede modificar el elemento");
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView2_DoubleClick(object sender, EventArgs e)
+        {
+            Shippers Ship = Cargadores[dataGridView2.CurrentCell.RowIndex];
+            textBox4.Text = Ship.CompanyName;
+            textBox3.Text = Ship.Phone;
+        }
+
+        private void dataGridView1_Click(object sender, EventArgs e)
+        {
+            Categories Cat = Categorias[dataGridView1.CurrentCell.RowIndex];
+            textBox1.Text = Cat.CategoryName;
+            textBox2.Text = Cat.Description;
+        }
+
+        private void tabControl1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView2_Click(object sender, EventArgs e)
+        {
+            Shippers Ship = Cargadores[dataGridView2.CurrentCell.RowIndex];
+            textBox4.Text = Ship.CompanyName;
+            textBox3.Text = Ship.Phone;
+        }
+    }
+}
